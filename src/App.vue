33b0aea6aa9280711d19d8fe8e1e554cc42a9574
Ingrid G <ing.gape18@gmail.com>
2023-04-26 16:07:24 -0500

    renderizado condicional

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,50 @@
 <template>
-  <main>
-    <!--<pre></pre> visualiza como json-->
-    <pre>{{ data }}</pre>
-  </main>
+    <main>
+        <!--<pre> visualiza como json-->
+        <!-- <pre>{{ data }}</pre> -->
+        <Card 
+            v-for="personajes in data" 
+            :key="personajes.id" 
+            :image="personajes.image"
+            :id="personajes.id" 
+            :name="personajes.name"
+            :actor="personajes.actor"
+            :house="personajes.house"
+        />
+    </main>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
+import Card from "./components/Card.vue";
 
 const data = ref([]);
 
 const getData = async () => {
-  const res = await fetch("https://hp-api.onrender.com/api/characters");
-  //Cuando se ejecute el fetch quiero que los datos con los q responda la API se guarden en una constante llamada json
-  const json = await res.json(); //para q lo formatee como jason y no lo pase como texto
-  console.log(json);
-  //se mapea el id para hacerlo manejable
-  const personajes = json.map((element, index) => {
-    element.id = index + 1;
-    return element;
-  });
-  console.log(personajes);
-  data.value = personajes;
+    const res = await fetch("https://hp-api.onrender.com/api/characters");
+
+    //Cuando se ejecute el fetch quiero que los datos con los q responda la API se guarden en una constante llamada json
+    const json = await res.json(); //para q lo formatee como json y no lo pase como texto
+    console.log(json);
+
+    //se mapea el id para hacerlo manejable
+    const personajes = json.map((element, index) => {
+        element.id = index + 1;
+        return element;
+    });
+    //console.log(personajes);
+
+    //Cambia el estado de data con el valor actual de id mapeado
+    data.value = personajes;
 };
 //console.log(getData()) //pending promise
+
 onMounted(async () => {
-  await getData();
+    //resuleve la promesa pendiente
+    await getData();
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+        <img :src="image" :alt="name"/> <!-- : para pasar como atributo -->
+        <span class="id">{{ id }}</span> -<!-- {{  }} para pasar como variable -->
+        <h3 class="name">{{ name }}</h3> 
+        <span class="actor">{{ actor }}</span> 
+        <span class="house">{{ house }}</span>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['id', 'name', 'actor', 'image', 'house']);
+
+</script> 
+
+<style scoped>
+.card{
+    width: 25%;
+    height: 25%;
+} 
+</style>
